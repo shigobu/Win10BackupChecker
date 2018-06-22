d919b7bbbfe503a66103d5510b71360a269a34be
shigobu <shigobu@shigobu.com>
2018-06-22 15:10:55 +0900

    メイン画面作成

--- バックアップはできます/Form1.cs
+++ バックアップはできます/Form1.cs
@@ -16,5 +16,30 @@ namespace バックアップはできます
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            //デフォルトでDesktop
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            //最初に選択するフォルダを指定する
+            //RootFolder以下にあるフォルダである必要がある
+            fbd.SelectedPath = @"C:";
+            //ユーザーが新しいフォルダを作成できるようにする
+            //デフォルトでTrue
+            fbd.ShowNewFolderButton = false;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                //選択されたフォルダを表示する
+                PassTextBox.Text = fbd.SelectedPath;
+            }
+        }
     }
 }
